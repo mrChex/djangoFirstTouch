03275e624b43727cb7aa60416e3cf49a430a98a8
greengrey <zgreengreyz@gmail.com>
2013-03-07 22:34:44 +0200

    some updates

---
+++ src/registration/forms.py
@@ -0,0 +1,11 @@
+from django import forms as forms
+
+
+
+class RegistrationForm(forms.Form):
+    email = forms.CharField(label=(u'E-mail'), max_length = 50)
+    username = forms.CharField(label=(u'Username'), max_length = 30)
+    password = forms.CharField(label=(u'Password'), max_length = 50)
+    fields = ["username", "password", "email"]
+
+reg = RegistrationForm()
