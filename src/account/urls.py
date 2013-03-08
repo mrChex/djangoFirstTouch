89a0704ef03071d4dec9cf1d92b6c0dfe6cddef0
greengrey <zgreengreyz@gmail.com>
2013-03-08 08:13:01 +0200

    some updates

---
+++ src/account/urls.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -8-
+from django.conf.urls import patterns, include, url
+from django.contrib.auth.views import login, logout
+
+urlpatterns = patterns('',
+                       (r'^login/$',  login),
+                       (r'^logout/$', logout),
+                       )
