de4886f369e0d72c20fc6b0a4e17b032d5894a98
Bogdan <zgreengreyz@gmail.com>
2013-03-21 11:29:43 +0200

    registration fix

--- src/account/urls.py
+++ src/account/urls.py
@@ -2,6 +2,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.views import login, logout
 
+
+
 urlpatterns = patterns('',
                        (r'^login/$',  login),
                        (r'^logout/$', logout),
